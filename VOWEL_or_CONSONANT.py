e74ce280a8638b5540ed8a1e41f8a0be6dfc656d
22P31A0433 <119719549+22P31A0433@users.noreply.github.com>
2023-08-16 21:31:30 +0530

    n=(input()) if(n=='a'or n=='e'or n=='i'or n=='o'or n=='u'):     print("VOWEL") elif(n=='A'or n=='E'or n=='I'or n=='O'or n=='U'):     print("VOWEL") else:     print("CONSONANT")

---
+++ VOWEL_or_CONSONANT.py
@@ -0,0 +1,7 @@
+n=(input())
+if(n=='a'or n=='e'or n=='i'or n=='o'or n=='u'):
+    print("VOWEL")
+elif(n=='A'or n=='E'or n=='I'or n=='O'or n=='U'):
+    print("VOWEL")
+else:
+    print("CONSONANT")
